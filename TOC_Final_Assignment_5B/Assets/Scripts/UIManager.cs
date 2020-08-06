cff4084c99c66e4a91e239cd64c9105cf8854d76
Afzaal <65043912+afzaal786-comsats@users.noreply.github.com>
2020-08-06 12:20:50 +0500

    final commit
    final commit

---
+++ TOC_Final_Assignment_5B/Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void PenguineGame()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+
+    public void HummingBirdGame()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+
+    public void MatchingParenthesis()
+    {
+        SceneManager.LoadScene("MatchingParenthesis");
+    }
+    public void Palindrome()
+    {
+        SceneManager.LoadScene("Palindrome");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Game_Menu");
+    
+    }
+    public void InstructorLink()
+    {
+        Application.OpenURL("http://www.niazilab.com/");
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+                Application.Quit();
+        #elif UNITY_WEBGL
+                Application.OpenURL("about:blank");
+        #endif
+    }
+}
---
+++ TOC_Final_Assignment_5B/Assets/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Splash1());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Splash1()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Game_Menu");
+
+
+    }
+}
